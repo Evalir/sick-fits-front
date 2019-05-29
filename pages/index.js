d5b07011cfe1271edcbdf35285b4357438d91ddc
evalir <hi.evalir@gmail.com>
2019-05-29 11:10:04 -0400

    Finish module one

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      Sup
+    </div>
+  )
+}
+
+export default Home
---
+++ pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      Sell
+    </div>
+  )
+}
+
+export default Sell
